414ba07c74f56cc1d253454aff5b01ef3829eadc
Catalin Patruica <catalin.patruica@xlab-iq.de>
2013-10-25 11:36:10 +0200

    initial commit
    Change-Id: I93e20f5bb1cd9aa367fec6be5ea96ac121ca27a0

---
+++ src/test/java/de/xlabiq/sample/test/maven/release/plugin/AppTest.java
@@ -0,0 +1,19 @@
+package de.xlabiq.sample.test.maven.release.plugin;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    @Test
+    public void suite()
+    {
+        assertEquals( 2, 2 );
+    }
+
+    
+}
